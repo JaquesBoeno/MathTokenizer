cba86aac002fc29d942ea88947f398b784422c47
JaquesBoeno <Samuray1326@proton.me>
2023-11-26 09:49:43 -0300

    add Multiplication and Division, in the correct order

--- internal/calc/calculator.go
+++ internal/calc/calculator.go
@@ -11,8 +11,44 @@ import (
 func Calculate(arr []token.Token) (float64, []token.Token, error) {
 	var result float64
 	tokens := arr
-	for i := 0; i <= len(tokens); i++ {
+	/*
 
+		  if tokens[i].Type == "Operator" && tokens[i].Value == "+" {
+						val, err := Sum(tokens[i-1], tokens[i+1])
+						if err == nil {
+							result = val
+							tokens = append(append(tokens[0:i-1], token.Token{Type: "Number", Value: fmt.Sprintf("%f", result)}), tokens[i+2:]...)
+							i = 0
+						} else {
+							return 0, tokens, err
+						}
+					}
+	*/
+	for i := 0; i <= len(tokens); i++ {
+		if i < len(tokens) {
+			if tokens[i].Type == "Operator" && tokens[i].Value == "*" {
+				val, err := Multiplication(tokens[i-1], tokens[i+1])
+				if err == nil {
+					result = val
+					tokens = append(append(tokens[0:i-1], token.Token{Type: "Number", Value: fmt.Sprintf("%f", result)}), tokens[i+2:]...)
+					i = 0
+				} else {
+					return 0, tokens, err
+				}
+			} else if tokens[i].Type == "Operator" && tokens[i].Value == "/" {
+				val, err := Division(tokens[i-1], tokens[i+1])
+				if err == nil {
+					result = val
+					tokens = append(append(tokens[0:i-1], token.Token{Type: "Number", Value: fmt.Sprintf("%f", result)}), tokens[i+2:]...)
+					i = 0
+				} else {
+					return 0, tokens, err
+				}
+			}
+		}
+	}
+
+	for i := 0; i <= len(tokens); i++ {
 		if i < len(tokens) {
 			if tokens[i].Type == "Operator" && tokens[i].Value == "+" {
 				val, err := Sum(tokens[i-1], tokens[i+1])
@@ -25,7 +61,6 @@ func Calculate(arr []token.Token) (float64, []token.Token, error) {
 				}
 			}
 		}
-
 	}
 	return result, tokens, nil
 }
@@ -33,11 +68,41 @@ func Calculate(arr []token.Token) (float64, []token.Token, error) {
 func Sum(left token.Token, right token.Token) (float64, error) {
 	rightV, errR := strconv.ParseFloat(left.Value, 64)
 	leftV, errL := strconv.ParseFloat(right.Value, 64)
+
 	if left.Type == "Number" && right.Type == "Number" {
 		if errL == nil && errR == nil {
 			return leftV + rightV, nil
 		}
 		return 0, errors.New(errL.Error() + errR.Error())
 	}
+
+	return 0, errors.New("Os valores tem que numéricos")
+}
+
+func Multiplication(left token.Token, right token.Token) (float64, error) {
+	rightV, errR := strconv.ParseFloat(left.Value, 64)
+	leftV, errL := strconv.ParseFloat(right.Value, 64)
+
+	if left.Type == "Number" && right.Type == "Number" {
+		if errL == nil && errR == nil {
+			return leftV * rightV, nil
+		}
+		return 0, errors.New(errL.Error() + errR.Error())
+	}
+
+	return 0, errors.New("Os valores tem que numéricos")
+}
+
+func Division(left token.Token, right token.Token) (float64, error) {
+	rightV, errR := strconv.ParseFloat(left.Value, 64)
+	leftV, errL := strconv.ParseFloat(right.Value, 64)
+
+	if left.Type == "Number" && right.Type == "Number" {
+		if errL == nil && errR == nil {
+			return leftV / rightV, nil
+		}
+		return 0, errors.New(errL.Error() + errR.Error())
+	}
+
 	return 0, errors.New("Os valores tem que numéricos")
 }
